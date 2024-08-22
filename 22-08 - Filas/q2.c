f8542fb234b6223745df6972fe678d24e2057e9d
Tico_Notebook <tgafa@cesar.school>
2024-08-22 10:38:54 -0300

    chore: Refactor code to use linked list functions for inserting nodes

--- 22-08 - Filas/q2.c
+++ 22-08 - Filas/q2.c
@@ -3,12 +3,37 @@
 
 typedef struct Node{
 
-    
+    char nome[30];
     struct Node *next;
 
 }Node;
 
+void inserir(Node **head, Node **tail, char nome[30]){
+
+    Node *novo = (Node*)malloc(sizeof(Node));
+
+    strcpy(novo->nome, nome);
+    novo ->next = NULL;
+
+    if (*head == NULL){
+
+        *head = *tail = novo;
+        return;
+
+
+    }else{
+        (*tail)->next = novo;
+    }
+    
+    *tail = novo;
+
+    
+}
+
 int main(){
+
+    Node *head = NULL;
+    Node *tail = NULL;
 
 
     return 0;
