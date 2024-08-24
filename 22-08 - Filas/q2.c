6e2b44f1cb3d054a9b88ab39a0b8981b7ea3af26
Tico_Notebook <tgafa@cesar.school>
2024-08-24 18:03:31 -0300

    Refactor code to include menu functionality for a medical clinic

--- 22-08 - Filas/q2.c
+++ 22-08 - Filas/q2.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+
+Crie um programa em C para uma clínica médica que precisa  automatizar o controle da ordem de atendimento de seus pacientes que deve possuir um menu com as seguintes funcionalidades:
+
+Clínica Médica
+
+=============
+
+(1) Incluir paciente
+
+(2) Realizar atendimento do paciente (remoção do paciente)
+
+(3) Consultar a posição atual do paciente pelo nome
+
+(4) Exibir a quantidade de pacientes já atendidos
+
+(5) Sair
+
+O critério de atendimento é a ordem de chegada. O paciente deve fornecer seu nome. Após isso, o paciente é adicionado ao sistema e deve aguardar sua vez. 
+
+
+**Para a estrutura da fila considere o seguinte struct: 
+
+typedef struct { char nome[30];  
+
+                           struct Node *prox; } Node;
+
+*/
+
 typedef struct Node {
     char nome[30];
     struct Node *next;
