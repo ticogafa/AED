3b97e77a3a11b50a5fb3432aaa5013cf09a381d0
Tico_Notebook <tgafa@cesar.school>
2024-08-27 09:45:09 -0300

    Refactor code to use linked list functions for inserting nodes into the stack

--- Fundamentos/pilha_01.c
+++ Fundamentos/pilha_01.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node{
+
+    int data;
+    struct Node *next;
+
+}Node;
+
+void inserir(Node **head, int data){
+
+    Node *novo = (Node*)malloc(sizeof(Node));
+
+}
+
 int main(){
 
-    
+
 
     return 0;
 
