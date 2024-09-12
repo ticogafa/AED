30ca5c7a43eaa4ed6702c33a19140e925dda9df7
ticogafa <tgafa@cesar.school>
2024-09-12 09:41:24 -0300

    Refactor insertion sort code: Update function parameter names for clarity

--- Fundamentos/InsertionSort.c
+++ Fundamentos/InsertionSort.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 void insertionSort(int arr[], int LEN) {
-    int i, aux, j;
-    for (i = 1; i < LEN; i++) {
-        aux = arr[i];
-        j = i - 1;
+    int i, aux, j; // i = index, aux = value, j = index
+    for (i = 1; i < LEN; i++) {// i = 1 because the first element is already sorted
+        aux = arr[i];// aux = value of the current element
+        j = i - 1;      // j = index of the previous element
 
-        while (j >= 0 && arr[j] > aux) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = aux;
+        while (j >= 0 && arr[j] > aux) {        // while the previous element is greater than the current element
+            arr[j + 1] = arr[j];    // move the previous element to the right
+            j = j - 1;            // move to the left
+        }     // end of the while loop
+        arr[j + 1] = aux;   // insert the current element in the right position
     }
 }
 
