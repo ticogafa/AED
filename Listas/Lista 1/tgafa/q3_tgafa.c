96b1e4510b88faf9d85903e57b8e619f2b1d22fd
ticogafa <tgafa@cesar.school>
2024-09-15 21:59:12 -0300

    Refactor code for flight controller: Add missing file header and include statements

---
+++ Listas/Lista 1/tgafa/q3_tgafa.c
@@ -0,0 +1,26 @@
+//3163 - Controlador de Vôo
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Node {
+
+    Node *next;
+
+} Node;
+
+int main(){
+
+    Node *head = NULL;
+    int P;
+
+    do{
+        scanf("%d", &P);
+        if(P == 0) break;
+        
+
+
+    }while(1);
+
+    return 0;
+}
