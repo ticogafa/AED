d16bcd6371505d65ca92f853ac9fdc256e04fe03
ticogafa <tgafa@cesar.school>
2024-09-24 09:02:58 -0300

    Refactor QuickSort.c: Implement Natacha's quicksort algorithm with timing information

--- Fundamentos/QuickSort.c
+++ Fundamentos/QuickSort.c
@@ -2,6 +2,65 @@
 #include <stdlib.h>
 #include <time.h>
 
+void quickSortNatacha(int v[], int inicio, int fim);
+int particao(int v[], int inicio, int fim);
+void troca(int v[], int i, int j);
+
+int main() {
+    int i, n = 10;
+    int vetor[n];
+    srand(time(NULL));
+    for(i = 0; i < n; i++) {
+        vetor[i] = rand() % 100;
+    }
+    printf("Vetor desordenado:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+    quickSortNatacha(vetor, 0, n - 1);
+    printf("\nVetor ordenado:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+
+    printf("\n");
+
+    printf("Tempo de execucao: %f segundos\n", (double)clock() / CLOCKS_PER_SEC);
+
+    return 0;
+}
+
+void quickSortNatacha(int v[], int inicio, int fim){
+    if(inicio<fim){
+        int indice = particao(v, inicio, fim);
+
+        quickSortNatacha(v, inicio, indice-1);
+        quickSortNatacha(v, indice+1, fim);
+    }
+}
+
+int particao(int v[], int inicio, int fim){
+    int pivo = v[inicio];
+    int i;
+    int indice = fim;
+
+    for(i = fim; i > inicio; i--){
+        if(v[i] >= pivo){
+            troca(v, i, indice);
+            indice--;
+        }
+    }
+    troca(v, inicio, indice);
+
+    return indice;
+}
+
+void troca(int v[], int i, int j){
+    int aux = v[i];
+    v[i] = v[j];
+    v[j] = aux;
+}
+
 void quickSort(int *vetor, int inicio, int fim) {
     int i, j, pivo, aux;
     i = inicio;
@@ -30,26 +89,3 @@ void quickSort(int *vetor, int inicio, int fim) {
     }
 }
 
-int main() {
-    int i, n = 10;
-    int vetor[n];
-    srand(time(NULL));
-    for(i = 0; i < n; i++) {
-        vetor[i] = rand() % 100;
-    }
-    printf("Vetor desordenado:\n");
-    for(i = 0; i < n; i++) {
-        printf("%d ", vetor[i]);
-    }
-    quickSort(vetor, 0, n - 1);
-    printf("\nVetor ordenado:\n");
-    for(i = 0; i < n; i++) {
-        printf("%d ", vetor[i]);
-    }
-
-    printf("\n");
-
-    printf("Tempo de execucao: %f segundos\n", (double)clock() / CLOCKS_PER_SEC);
-
-    return 0;
-}
