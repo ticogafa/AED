b2eba779da561951653c56f65ad174212ef135e7
ticogafa <tgafa@cesar.school>
2024-10-01 10:08:25 -0300

    Implement multiplication functionality for circular linked list
    This commit adds a new functionality to a C program that manipulates numbers in a circular singly linked list. The program now includes a function that calculates and prints the multiplication of pairs of nodes in the list. If there is an odd number of nodes, the remaining node is multiplied by itself. If the list has less than 2 nodes, an error message is displayed.
    
    Function prototype: void multiplicacao(Node *head, Node *tail);

---
+++ 01-10 - Revisão/q1.c
@@ -0,0 +1,53 @@
+//01 - Um determinado programa será implementado em C e deve permitir manipular números em uma lista circular 
+//simplesmente encadeada, mas, possui apenas a função que insere nós. 
+//      Inclua no programa uma nova funcionalidade que imprima a multiplicação dos valores de duplas de nós 
+//que estão nesta lista. Se tiver uma quantidade ímpar de nós (sobrando um nó), para o nó restante, multiplique 
+//o valor dele por ele próprio. E se a lista tiver menos que 2 nós, exiba a seguinte mensagem: “Tamanho da lista é insuficiente :(”.  
+//     Protótipo da função: void multiplicacao(Node *head, Node *tail);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+
+    int data;
+    struct Node *next;
+} Node;
+
+void multiplicacao(Node *head, Node *tail){
+
+ if (head == NULL || tail == NULL) {
+        printf("Tamanho da lista é insuficiente :(\n");
+        return;
+    }
+    Node *aux = head;
+    int cont = 0;
+
+    do{
+        cont++;
+        aux = aux->next;
+        
+    }while(aux!=head);
+
+    if(cont<2){
+        printf("Tamanho da lista é insuficiente :(");
+
+    }else if(cont%2 == 0){
+        aux = head;
+        do{
+            int multiplicacao = aux->data * aux->next->data;
+            printf("%d", multiplicacao);
+            aux = aux->next->next;
+        }while(aux!=head);
+    }else{
+        aux = head;
+        while(aux!=tail){
+            int multiplicacao = aux->data * aux->next->data;
+            printf("%d", multiplicacao);
+            aux = aux->next->next;
+        }
+        int multiplicacao = aux->data * aux->data;
+        printf("%d", multiplicacao);
+    }
+
+}
