22497c61151705d9cca627cd51c7ee1fe2583e8a
ticogafa <tgafa@cesar.school>
2024-10-07 15:41:05 -0300

    .

---
+++ Prova1/InsertionSort.c
@@ -0,0 +1,7 @@
+//6. Insertion Sort
+//Um programa C armazena números em um vetor e usa o algoritmo de ordenação Insertion Sort. 
+//Adapte o algoritmo para que ele também retorne o número total de comparações feitas 
+//durante o processo de ordenação.
+
+#include <stdio.h>
+#include <stdlib.h>
