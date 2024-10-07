d911960a5623824089cdd404c341ce16959a9bae
ticogafa <tgafa@cesar.school>
2024-10-07 15:03:50 -0300

    .

---
+++ Prova1/ListaCircularDuplamenteEncadeada.c
@@ -0,0 +1,4 @@
+//5. Lista Circular Duplamente Encadeada
+//Considere um programa que utiliza uma lista circular duplamente encadeada 
+//para armazenar números inteiros. Crie uma nova funcionalidade que identifique 
+//o segundo maior valor da lista. Se houver menos que 2 elementos, exiba: "Tamanho da lista é insuficiente :(".
