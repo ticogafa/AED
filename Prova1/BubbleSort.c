78aa700aac2ab93fdc5b51613a6d39ce5dabf181
ticogafa <tgafa@cesar.school>
2024-10-07 23:20:43 -0300

    .

---
+++ Prova1/BubbleSort.c
@@ -0,0 +1,28 @@
+//7. Bubble Sort
+
+//Um programa C utiliza o algoritmo de ordenação Bubble Sort para ordenar um vetor de números inteiros. 
+//Adicione uma função que retorne o número de trocas realizadas durante a ordenação. Se o vetor já 
+//estiver ordenado, exiba a mensagem "Nenhuma troca necessária :)".
+
+//Protótipo da função: int bubble_sort_trocas(int arr[], int n);
+
+#include <stdio.h>
+
+int bubble_sort_trocas(int arr[], int n);
+
+int main(){
+
+    int arr[] = {10,7,6,875,345,23,23425,2,235434,43,234,236,324367,56,34};
+    int LEN = sizeof(arr)/sizeof(arr[0]);
+
+
+
+    return 0;
+}
+
+
+int bubble_sort_trocas(int arr[], int n){
+
+    
+
+}
