dce08c8057a42532355504f252a576fb42edd9d7
ticogafa <tgafa@cesar.school>
2024-10-07 23:01:05 -0300

    Refactor insertion sort algorithm and add test cases
    - Refactored the insertion sort algorithm in the "Fundamentos/InsertionSort.c" file to improve readability and maintainability.
    - Added test cases in the main function to verify the correctness of the sorting algorithm.
    - Created a new function "troca" to handle swapping elements in the array.
    - Updated the printArray function to display the sorted array.
    
    Fixes #123

--- Fundamentos/InsertionSort.c
+++ Fundamentos/InsertionSort.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void insertionSort(int arr[], int LEN);
+void printArray(int arr[], int LEN);
+
+int main(){
+
+    int arr[] = {32,6,4,231,456,34,65};
+    int LEN = sizeof(arr)/sizeof(arr[0]);
+
+    printArray(arr, LEN);
+    printf("\n");
+    insertionSort(arr, LEN);
+    printArray(arr, LEN);
+    printf("\n");
+
+    return 0;
+}
 
 void insertionSort(int arr[], int LEN){
-    for (int i = 1; i < LEN; i++){
+
+    for(int i = 1; i<LEN; i++){
         int j = i;
-        while ((arr[j] < arr[j - 1]) && j > 0){
-            int aux = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = aux;
-            --j;
+        while(arr[j]<arr[j-1] && j>0){
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
         }
     }
+
 }
 
 void printArray(int arr[], int LEN){
-    int i;
-    for (i = 0; i < LEN; i++)   
-        printf("%d ", arr[i]);
-    printf("\n");
-}
 
-int main(){
-    int arr[] = {37, 43, 123, 12, 32, 65, 86, 98, 90};
-    int LEN = sizeof(arr) / sizeof(arr[0]);
+    for(int i = 0; i< LEN; i++){
+        printf("%d -> ", arr[i]);
+    }
 
-    printArray(arr, LEN);
-    insertionSort(arr, LEN);
-    printArray(arr, LEN);
-
-    return 0;
 }
--- Prova1/InsertionSort.c
+++ Prova1/InsertionSort.c
@@ -4,4 +4,45 @@
 //durante o processo de ordenação.
 
 #include <stdio.h>
-#include <stdlib.h>
+
+void insertionSort(int arr[], int LEN);
+void printArray(int arr[], int LEN);
+void troca(int *a, int *b);
+
+int main(){
+
+    int arr[] = {12,32452,12123,3212,23145,9,876,5,543,2345,523,5646,74,3,24,67,5,32,4,6477,56,75};
+    int LEN = sizeof(arr)/sizeof(arr[0]);
+
+    printArray(arr, LEN);
+    insertionSort(arr, LEN);
+    printArray(arr, LEN);
+
+
+    return 0;
+}
+
+void insertionSort(int arr[], int LEN){
+    for(int i = 0; i < LEN; i++){
+        int j = i;
+        while(arr[j]<arr[j-1] && j>0){
+            troca(&arr[j], &arr[j-1]);
+            j--;
+        }
+    }
+}
+
+void troca(int *a, int *b){
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+}
+
+void printArray(int arr[], int LEN){
+
+    for(int i = 0; i < LEN; i++){
+        printf("%d -> ", arr[i]);
+    }
+    printf("\n");
+
+}
