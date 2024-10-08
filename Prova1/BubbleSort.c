d71a0d172e1cedc4fe1f7b8a91c5999b914a50c3
ticogafa <tgafa@cesar.school>
2024-10-07 23:41:30 -0300

    Refactor BubbleSort algorithm and add test cases

--- Prova1/BubbleSort.c
+++ Prova1/BubbleSort.c
@@ -8,21 +8,59 @@
 
 #include <stdio.h>
 
-int bubble_sort_trocas(int arr[], int n);
+int bubble_sort_trocas(int arr[], int LEN);
+void trocar(int *a, int *b);
+void printArray(int arr[], int LEN);
 
 int main(){
 
     int arr[] = {10,7,6,875,345,23,23425,2,235434,43,234,236,324367,56,34};
     int LEN = sizeof(arr)/sizeof(arr[0]);
 
+    printArray(arr, LEN);
+    bubble_sort_trocas(arr, LEN);
+    printArray(arr, LEN);
 
 
     return 0;
 }
 
+void printArray(int arr[], int LEN){
 
-int bubble_sort_trocas(int arr[], int n){
 
-    
+    for(int i = 0; i<LEN; i++){
+        printf("%d -> ", arr[i]);
+    }
 
+    printf("\n");
+}
+
+int bubble_sort_trocas(int arr[], int LEN){
+
+    int n = 1, troca = 1;
+    int trocaRealizadas = 0;
+
+    while(troca == 1 && n<=LEN){
+        troca = 0;
+        for(int i = 0; i<LEN-1; i++){
+            if(arr[i]>arr[i+1]){
+                trocar(&arr[i], &arr[i+1]);
+                troca = 1;
+                trocaRealizadas++;
+            }
+        }
+        ++n;
+    }
+
+    printf("\nNenhuma troca necessária :)\n");
+    printf("\nQuantidade de trocas realizadas: %d\n", trocaRealizadas);
+
+    return trocaRealizadas;
+}
+
+void trocar(int *a, int *b){
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
