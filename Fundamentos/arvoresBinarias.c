ba408595708b5c18b0b72610792743193a31ac2a
ticogafa <tgafa@cesar.school>
2024-10-17 09:48:53 -0300

    .

--- Fundamentos/arvoresBinarias.c
+++ Fundamentos/arvoresBinarias.c
@@ -4,9 +4,10 @@
 typedef struct Node {
     int chave;
     struct Node *esquerda, *direita;
-}Node;
+} Node;
 
 void inserir(Node **raiz, int chave);
+int altura(Node *raiz);
 void preordem(Node *raiz);
 void liberarArvore(Node *raiz);
 Node* encontrarMaior(Node *raiz);
@@ -16,8 +17,7 @@ void inordem(Node *raiz);
 void posordem(Node *raiz);
 int buscar(Node *raiz, int chave);
 
-int main(){
-
+int main() {
     Node *root = NULL;
     inserir(&root, 5);
     inserir(&root, 2);
@@ -29,66 +29,67 @@ int main(){
     printf("\nBuscar número 4: %d\n", buscar(root, 4));
     preordem(root);
     printf("\n");
+    printf("\nAltura da árvore: %d\n", altura(root));
     remover(&root, 4);
     printf("\nBuscar número 4: %d\n", buscar(root, 4));
     preordem(root);
     printf("\n");
+    printf("\nAltura da árvore: %d\n", altura(root));
     liberarArvore(root);
 
-    return 0;
 
+    return 0;
 }
 
-int buscar(Node *raiz, int chave){
-
-    if(raiz == NULL) return 0;
-    else if(raiz->chave == chave) return 1;
-    else if(chave < raiz->chave) return buscar(raiz->esquerda, chave);
+int buscar(Node *raiz, int chave) {
+    if (raiz == NULL) return 0;
+    else if (raiz->chave == chave) return 1;
+    else if (chave < raiz->chave) return buscar(raiz->esquerda, chave);
     else return buscar(raiz->direita, chave);
 }
- 
-void inserir(Node **raiz, int chave){
 
-    if(*raiz == NULL){
+void inserir(Node **raiz, int chave) {
+    if (*raiz == NULL) {
         Node *newNode = (Node*)malloc(sizeof(Node));
+        if (newNode == NULL) {
+            fprintf(stderr, "Erro ao alocar memória\n");
+            exit(EXIT_FAILURE);
+        }
         newNode->chave = chave;
         newNode->esquerda = NULL;
         newNode->direita = NULL;
         *raiz = newNode;
-    }else{
-        if((*raiz)->chave > chave){
+    } else {
+        if ((*raiz)->chave > chave) {
             inserir(&(*raiz)->esquerda, chave);
-        }else if((*raiz)->chave < chave){
+        } else if ((*raiz)->chave < chave) {
             inserir(&(*raiz)->direita, chave);
         }
     }
- 
 }
 
-void preordem(Node *raiz){
-
-    if(raiz != NULL){
+void preordem(Node *raiz) {
+    if (raiz != NULL) {
         printf("%d ", raiz->chave);
         preordem(raiz->esquerda);
         preordem(raiz->direita);
     }
-
 }
 
 void inordem(Node *raiz) {
-  if (raiz != NULL) {
-    inordem(raiz->esquerda);
-    printf("%d ", raiz->chave);
-    inordem(raiz->direita);
-  }
+    if (raiz != NULL) {
+        inordem(raiz->esquerda);
+        printf("%d ", raiz->chave);
+        inordem(raiz->direita);
+    }
 }
 
 void posordem(Node *raiz) {
-  if (raiz != NULL) {
-    posordem(raiz->esquerda);
-    posordem(raiz->direita);
-    printf("%d ", raiz->chave);
-  }
+    if (raiz != NULL) {
+        posordem(raiz->esquerda);
+        posordem(raiz->direita);
+        printf("%d ", raiz->chave);
+    }
 }
 
 void liberarArvore(Node *raiz) {
@@ -110,56 +111,60 @@ Node *encontrarMaior(Node *raiz) {
 }
 
 Node *MaiorDireita(Node **raiz) {
-  if ((*raiz)->direita != NULL)
-    return MaiorDireita(&(*raiz)->direita);
-  else {
-    Node *aux = *raiz;
-    if ((*raiz)->esquerda != NULL) {
-      *raiz = (*raiz)->esquerda;
-    } else {
-      *raiz = NULL;
+    if ((*raiz)->direita != NULL)
+        return MaiorDireita(&(*raiz)->direita);
+    else {
+        Node *aux = *raiz;
+        if ((*raiz)->esquerda != NULL) {
+            *raiz = (*raiz)->esquerda;
+        } else {
+            *raiz = NULL;
+        }
+        return aux;
     }
-    return aux;
-  }
 }
 
 void remover(Node **raiz, int numero) {
-  if (*raiz == NULL) {
-    printf("Numero nao existe na arvore!");
-    return;
-  }
-  if (numero < (*raiz)->chave)
-    remover(&(*raiz)->esquerda, numero);
-  else if (numero > (*raiz)->chave)
-    remover(&(*raiz)->direita, numero);
-  else {
-    Node *aux = *raiz;
-    // 01 - no sem filhos
-    if (((*raiz)->esquerda == NULL) && ((*raiz)->direita == NULL)) {
-      free(aux);
-      (*raiz) = NULL;
-    } else {
-      // 02 - no com filho direito
-      if ((*raiz)->esquerda == NULL) {
-        (*raiz) = (*raiz)->direita;
-        aux->direita = NULL;
-        free(aux);
-      } else {
-        // 02 - no com filho esquerdo
-        if ((*raiz)->direita == NULL) {
-          (*raiz) = (*raiz)->esquerda;
-          aux->esquerda = NULL;
-          free(aux);
-        } else {
-          // 03 - no com dois filhos
-          aux = MaiorDireita(&(*raiz)->esquerda);
-          aux->esquerda = (*raiz)->esquerda;
-          aux->direita = (*raiz)->direita;
-          free(*raiz);
-          *raiz = aux;
-        }
-      }
+    if (*raiz == NULL) {
+        printf("Numero nao existe na arvore!\n");
+        return;
     }
-  }
+    if (numero < (*raiz)->chave) {
+        remover(&(*raiz)->esquerda, numero);
+    } else if (numero > (*raiz)->chave) {
+        remover(&(*raiz)->direita, numero);
+    } else {
+        Node *aux = *raiz;
+        // 01 - no sem filhos
+        if ((*raiz)->esquerda == NULL && (*raiz)->direita == NULL) {
+            free(aux);
+            *raiz = NULL;
+        } else if ((*raiz)->esquerda == NULL) { // 02 - no com filho direito
+            *raiz = (*raiz)->direita;
+            free(aux);
+        } else if ((*raiz)->direita == NULL) { // 02 - no com filho esquerdo
+            *raiz = (*raiz)->esquerda;
+            free(aux);
+        } else { // 03 - no com dois filhos
+            aux = MaiorDireita(&(*raiz)->esquerda);
+            aux->esquerda = (*raiz)->esquerda;
+            aux->direita = (*raiz)->direita;
+            free(*raiz);
+            *raiz = aux;
+        }
+    }
 }
 
+int altura(Node *raiz) {
+    if (raiz == NULL) {
+        return -1;
+    } else {
+        int alturaEsquerda = altura(raiz->esquerda);
+        int alturaDireita = altura(raiz->direita);
+        if (alturaEsquerda > alturaDireita) {
+            return alturaEsquerda + 1;
+        } else {
+            return alturaDireita + 1;
+        }
+    }
+}
