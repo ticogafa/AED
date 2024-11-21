6b1aa8eabc0bb34ee69d69ceafe91fdd4a4a1e39
ticolinux <tiago.gurgel2006@gmail.com>
2024-11-21 02:59:40 -0300

    Refactor Merge Sort implementation and remove unused treino.c file

--- Fundamentos/MergeSort.c
+++ Fundamentos/MergeSort.c
@@ -1,62 +1,68 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define TAMANHO 10
 
-void mergesort(int inicio, int fim, int v[]);
-void intercala(int inicio, int meio, int fim, int v[]);
+void mergeSort(int inicio, int final, int v[]);
+void intercala(int inicio, int meio, int final, int v[]);
 
 int main(){
 
-    int v[TAMANHO] = {38, 27, 43, 3, 9, 82, 10, 15, 6, 1};
-    int i;
 
-    printf("Array antes da ordenação:\n");
-    for (i = 0; i < TAMANHO; i++) {
+    int v[TAMANHO] = {
+        1,
+        5,
+        3,
+        45,
+        65,
+        43,
+        32,
+        12,
+        98,
+        87
+    };
+
+    for(int i = 0; i<TAMANHO; i++){
         printf("%d ", v[i]);
     }
     printf("\n");
 
-    mergesort(0, TAMANHO - 1, v);
+    mergeSort(0, TAMANHO-1, v);
 
-    printf("Array após a ordenação:\n");
-    for (i = 0; i < TAMANHO; i++) {
+
+    for(int i = 0; i<TAMANHO; i++){
         printf("%d ", v[i]);
     }
     printf("\n");
 
     return 0;
-
 }
 
-void mergesort (int inicio, int fim, int v[]) {
-    if (inicio < fim) {
-        int meio = (inicio + fim)/2;
-        mergesort (inicio, meio, v);
-        mergesort (meio+1, fim, v);
-        intercala (inicio, meio, fim, v);
-    }
- }
 
- void intercala (int inicio, int meio, int fim, int v[]) {
-        int inicio_v01 = inicio;
-        int inicio_v02 = meio+1;
-        int poslivre = 0;
-        int aux[TAMANHO];
-        
-        while (inicio_v01 <= meio && inicio_v02 <= fim) {
-            if (v[inicio_v01] <= v[inicio_v02])
-                aux[poslivre++] = v[inicio_v01++];
-            else
-                aux[poslivre++] = v[inicio_v02++];
-        }
-        //se existirem núm. em v[inicio_v01] que não foram intercalados
-        while (inicio_v01 <= meio)
-            aux[poslivre++] = v[inicio_v01++];
-        //se existirem núm. em v[inicio_v02] que não foram intercalados
-        while (inicio_v02 <= fim)
-            aux[poslivre++] = v[inicio_v02++];
-        //retorna os valores do vetor aux para o vetor v
-        for (inicio_v01 = inicio; inicio_v01 <= fim; inicio_v01++)
-            v[inicio_v01] = aux[inicio_v01-inicio];
+void mergeSort(int inicio, int final, int v[]){
+    if(inicio < final){
+
+        int meio = (inicio + final)/2;
+
+        mergeSort(inicio, meio, v);
+        mergeSort(meio + 1, final, v);
+        intercala(inicio, meio, final, v);
+    }
+}
+
+void intercala(int inicio, int meio, int final, int v[]){
+
+    int inicio1 = inicio;
+    int inicio2 = meio + 1;
+    int posLivre = 0;
+    int aux[TAMANHO];
+
+    while(inicio1<=meio && inicio2<=final){
+        if(v[inicio1]<=v[inicio2]) aux[posLivre++] = v[inicio1++];
+        else aux[posLivre++] = v[inicio2++];
+    }
+
+    while(inicio1<=meio) aux[posLivre++] = v[inicio1++];
+    while(inicio2<=final) aux[posLivre++] = v[inicio2++];
+
+    for(inicio1 = inicio; inicio1 <= final; inicio1++) v[inicio1] = aux[inicio1 - inicio];
 }
